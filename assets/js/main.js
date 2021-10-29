b2370816b337059fbcf694d16396f0d3d37dd902
Valerio Corda <corda.valerio05@GMAIL.COM>
2021-10-29 12:43:12 +0200

    Installation and launch of Vue Js
    Created the instance and the new data structure (array of objects)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,40 @@
 /* Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente. */
+//collegamento Vue Js
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'SLIDER',
+        slides: [{
+            image: 'img/01.jpg',
+            title: 'Svezia',
+            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+        }, {
+            image: 'img/02.jpg',
+            title: 'Svizzera',
+            text: 'Lorem ipsum'
+        }, {
+            image: 'img/03.jpg',
+            title: 'Gran Bretagna',
+            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+        }, {
+            image: 'img/04.jpg',
+            title: 'Germania',
+            text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
+        }, {
+            image: 'img/05.jpg',
+            title: 'Paradise',
+            text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+        }, ]
+    }
+})
+
+
+
+
 
 
 //vecchio esercizio
-/* 
-const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
-];
-
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
-]
-
-const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-] */
-
 /*     `<div class="item">
                     <img src="${items[i]}" alt="">
                     <div class ="text">
